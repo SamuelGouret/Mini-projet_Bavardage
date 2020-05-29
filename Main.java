03b9233125d146d567fa24810905a8c0f481d0fa
SamuelGouret <56953297+SamuelGouret@users.noreply.github.com>
2020-05-29 18:14:01 +0200

    Main executable

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package finales;
 import javax.swing.JFrame;
 
 public class Main {
-	
+	// Créé un concierge à l'execution
 	public static void main(String[] args) {
 	Concierge c= new Concierge();    
 	InterConcierge ic = new InterConcierge(c);
